a0cf62e4cf816a11c0bb3f984d1fd37828e463cb
laadruturaj <laadruturaj@gmail.com>
2024-05-08 23:21:45 +0530

    .

---
+++ C++07.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+void update(int *a,int *b) {
+    // Complete this function
+    int sum=*a+*b;
+    int asum=abs(*a-*b);   
+    *a=sum;
+    *b=asum; 
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
